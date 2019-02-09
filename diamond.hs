bb453cead7e1cb320c6ba6b928c6a03a8f3bdcf8
nayemmajhar <nayemmajhar@gmail.com>
2019-02-09 03:46:07 +0100

    Display Diamond with IO in Haskell

---
+++ diamond.hs
@@ -0,0 +1,23 @@
+{-
+Implement a function diamond :: Int -> IO () that returns an IO action that outputs
+a diamond of given size (side length). For example, the executions of diamond 0, 
+diamond 1, diamond 2 and diamond 3
+-}
+
+diamond :: Int -> IO ()
+diamond n
+    | n <= 0 = return ()
+    | otherwise = putStr . unlines $ topStar n ++ middleStar n : bottomStar n
+
+topStar n = map (addStar n) [1..n-1] 
+middleStar n = addStar n n
+bottomStar n = reverse (topStar n)
+
+addStar :: Int -> Int -> String
+addStar n i
+    | i == 1 = (addSpace (n - i)) ++ "*"
+    | otherwise = (addSpace (n - i)) ++ "*" ++ (addSpace (2*i - 3)) ++ "*" ++ (addSpace (n - i))
+
+addSpace m = replicate m ' '
+
+
